4f211a351dc04fcdbbf2823caeac69c34f6909d1
qianyi liu <liuqianyi125@gmail.com>
2024-10-21 17:37:04 +0800

    add label test

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include "uart.h"
 extern void ldr_test(void);
 extern void memcpy_32bytes(void);
+extern void label_test(void);
 
 int main()
 {
 	uart_init();
 	uart_send_string("Hello World.\n");
+
+	label_test();
 	ldr_test();
 	memcpy_32bytes();
 	return 0;
