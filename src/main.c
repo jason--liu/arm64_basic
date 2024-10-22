add4080ad9db69afbb3b850c7d97051c9656779f
qianyi liu <liuqianyi125@gmail.com>
2024-10-22 16:12:07 +0800

    Add add/shift/C flag test cases

--- src/main.c
+++ src/main.c
@@ -3,12 +3,14 @@ extern void ldr_test(void);
 extern void memcpy_32bytes(void);
 extern void label_test(void);
 extern void mov_test(void);
+extern void add_test(void);
 
 int main()
 {
 	uart_init();
 	uart_send_string("Hello World.\n");
 
+	add_test();
 	mov_test();
 	label_test();
 	ldr_test();
