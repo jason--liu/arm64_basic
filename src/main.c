a53a3f5d16952b9a599cb56999d7a5259c42d1eb
qianyi liu <liuqianyi125@gmail.com>
2024-10-22 13:47:06 +0800

    add mov test

--- src/main.c
+++ src/main.c
@@ -2,12 +2,14 @@
 extern void ldr_test(void);
 extern void memcpy_32bytes(void);
 extern void label_test(void);
+extern void mov_test(void);
 
 int main()
 {
 	uart_init();
 	uart_send_string("Hello World.\n");
 
+	mov_test();
 	label_test();
 	ldr_test();
 	memcpy_32bytes();
