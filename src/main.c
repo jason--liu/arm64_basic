0259147bb1b8a70a1f0ac1ee30160810889d3296
qianyi liu <liuqianyi125@gmail.com>
2024-10-23 10:29:31 +0800

    add bitfield operation instruction

--- src/main.c
+++ src/main.c
@@ -4,12 +4,14 @@ extern void memcpy_32bytes(void);
 extern void label_test(void);
 extern void mov_test(void);
 extern void add_test(void);
+extern void bit_test(void);
 
 int main()
 {
 	uart_init();
 	uart_send_string("Hello World.\n");
 
+	bit_test();
 	add_test();
 	mov_test();
 	label_test();
